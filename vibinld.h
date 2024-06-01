df8e9ac7aacbfa09a893972bbe9029d7b2bb8623
almix1 <avmxlv@gmail.com>
2024-06-01 15:06:17 +0300

    Update vibinld.h

--- vibinld.h
+++ vibinld.h
@@ -4,7 +4,7 @@
 #define MAXVIB 4.5
 
 enum garmony {one = 1, two = 2, three = 3, four = 4, five = 5,
-	six = 6, seven = 7, eight = 8, nine = 9, ten = 10};
+    six = 6, seven = 7, eight = 8, nine = 9, ten = 10};
 
 enum {lenbear = 32, lenfile = 64};
 

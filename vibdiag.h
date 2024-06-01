efc4551366ce548472b7eda23173c4f3b5afc844
almix1 <avmxlv@gmail.com>
2024-06-01 14:39:34 +0300

    Add vibdiag.h

---
+++ vibdiag.h
@@ -0,0 +1,8 @@
+#ifndef VIB_H_SENTRY
+#define VIB_H_SENTRY
+
+void skz(double line_in_spectr, double *y_spectr);
+void diagnostics(int f_r, double *x_spectr, double *y_spectr);
+void diagnostics_bear(int f_r, double *x_spectr, double *y_spectr, char *numbear);
+
+#endif
